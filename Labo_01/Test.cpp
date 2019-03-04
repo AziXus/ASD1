bbbbf3fdcf528eb7cd6dc998e321be7fa9b7322c
Robin Müller <rm.muller@bluewin.com>
2019-03-04 08:32:50 +0100

    Test.h & cpp created

---
+++ Labo_01/Test.cpp
@@ -0,0 +1,118 @@
+#include "Test.h"
+
+#include <random>
+#include <ctime>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+
+using namespace std;
+
+
+vector<int> generateVector(size_t size) {
+    vector<int> v(size);
+
+    for (int i = 0; i < size; ++i) {
+        v[i] = i;
+    }
+
+    random_shuffle(v.begin(), v.end());
+
+    return v;
+}
+
+//Identique à trier2 en retournant le nombre d'itérations
+unsigned trier2(vector<int>& v) {
+    if(v.empty()) return 0;
+
+    unsigned nbIterations = 0;
+    for(int a : v) {
+        auto j = v.begin();
+        auto k = j++;
+        for(; j != v.end(); k = j++ ) {
+            ++nbIterations;
+            if (*k > *j)
+                swap(*k, *j);
+        }
+    }
+
+    return nbIterations;
+}
+
+//Idem a chercherSiContient2 mais avec le nombre d'itérations en retour
+unsigned chercherSiContient2(const vector<int>& v, int val) {
+    auto first = v.begin();
+    auto last = v.end();
+
+    unsigned nbIterations = 0;
+    while( first != last ) {
+        ++nbIterations;
+
+        auto mid = first + ( last - first ) / 2;
+        if(*mid == val) {
+            return nbIterations;
+        } else if(*mid < val) {
+            first = mid+1;
+        } else {
+            last = mid;
+        }
+    }
+    return nbIterations;
+}
+
+//Fonction identique à g2 mais retourne le nombre d'itérations
+unsigned g2(vector<int>& v) {
+    unsigned nbIterations = 0;
+    for(size_t i = 0; i < v.size(); ++i) {
+        for(size_t j = v.size() - 1; j > 0; j /= 2 ) {
+            ++nbIterations;
+            v.at(i) += v.at(j);
+        }
+    }
+    return nbIterations;
+}
+
+void testTrier(int nbValeurs, int base) {
+    cout << fixed << setprecision(2);
+
+    cout << "Fonction : trier()\n";
+    for (int i = 1; i <= nbValeurs; ++i) {
+        vector<int> v = generateVector((size_t)pow(base, i));
+
+        cout << "Iterations pour n = " << v.size() << " (" << base << "^" << i  << ") : " << trier2(v) << endl;
+    }
+}
+
+void testChercherSiContient(int nbValeurs, int base, int nbTests) {
+    cout << fixed << setprecision(2);
+
+    cout << "Fonction : chercherSiContient()\n";
+    for (int i = 1; i <= nbValeurs; ++i) {
+        vector<int> v = generateVector((size_t)pow(base, i));
+        sort(v.begin(), v.end());
+
+        double averageIterations = 0.;
+        cout << "Iterations pour n = " << v.size() << " (" << base << "^" << i  << ") : ";
+
+        //Calcul la moyenne pour nbTests valeurs
+        for (int j = 1; j <= nbTests; ++j) {
+            //La valeur recherché à 1 chance sur 10 de ne pas être dans le vecteur.
+            unsigned nbIterations = chercherSiContient2(v, rand() % (v.size() + v.size() / 10));
+
+            averageIterations += (double)nbIterations / nbTests;
+        }
+
+        cout << averageIterations << endl;
+    }
+}
+
+void testG(int nbValeurs, int base) {
+    cout << fixed << setprecision(2);
+
+    cout << "Fonction : g()\n";
+    for (int i = 1; i <= nbValeurs; ++i) {
+        vector<int> v = generateVector((size_t)pow(base, i));
+
+        cout << "Iterations pour n = " << v.size() << " (" << base << "^" << i  << ") : " << g2(v) << endl;
+    }
+}
--- Labo_01/main.cpp
+++ Labo_01/main.cpp
@@ -13,15 +13,8 @@
 #include <random>
 #include <ctime>
 #include <iostream>
-#include <iomanip>
-#include <limits>
-#include <algorithm>
-#include <chrono>
 
-void testChercherPosition(int nbTests);
-void testTrier(int nbValeurs, int base, int nbTests);
-void testChercherSiContient(int nbValeurs, int base, int nbTests);
-void testG(int nbValeurs, int base, int nbTests);
+#include "Test.h"
 
 using namespace std;
 
@@ -51,7 +44,7 @@ size_t chercherPosition( const vector<int>& v , int val ) {
  *
  * @param v vector à trier
  */
-void trier( vector<int>& v ) {
+void trier(vector<int>& v) {
     if(v.empty()) return;
 
     for(int a : v) {
@@ -61,24 +54,6 @@ void trier( vector<int>& v ) {
             if(*k > *j )
                 swap(*k, *j);
     }
-}
-
-//Identique à trier en retournant le nombre d'itérations
-unsigned trier2( vector<int>& v ) {
-    if(v.empty()) return 0;
-
-    unsigned nbIterations = 0;
-    for(int a : v) {
-        auto j = v.begin();
-        auto k = j++;
-        for(; j != v.end(); k = j++ ) {
-            ++nbIterations;
-            if (*k > *j)
-                swap(*k, *j);
-        }
-    }
-
-    return nbIterations;
 }
 
 /**
@@ -92,7 +67,7 @@ unsigned trier2( vector<int>& v ) {
  * @param val valeur à chercher
  * Retourne true si la valeur est contenue dans le vector, false sinon.
  */
-bool chercherSiContient( const vector<int>& v , int val ) {
+bool chercherSiContient(const vector<int>& v, int val) {
     auto first = v.begin();
     auto last = v.end();
 
@@ -107,27 +82,6 @@ bool chercherSiContient( const vector<int>& v , int val ) {
         }
     }
     return false;
-}
-
-//Idem a chercherSiContient mais avec le nombre d'itérations en retour
-unsigned chercherSiContient2( const vector<int>& v , int val ) {
-    auto first = v.begin();
-    auto last = v.end();
-
-    unsigned nbIterations = 0;
-    while( first != last ) {
-        ++nbIterations;
-
-        auto mid = first + ( last - first ) / 2;
-        if(*mid == val) {
-            return nbIterations;
-        } else if(*mid < val) {
-            first = mid+1;
-        } else {
-            last = mid;
-        }
-    }
-    return nbIterations;
 }
 
 /**
@@ -148,24 +102,12 @@ unsigned long long f( unsigned n ) {
  *
  * @param v
  */
-void g( vector<int>& v ) {
+void g(vector<int>& v) {
     for(size_t i = 0; i < v.size(); ++i) {
         for(size_t j = v.size() - 1; j > 0; j /= 2 ) {
             v.at(i) += v.at(j);
         }
     }
-}
-
-//Fonction identique à g mais retourne le nombre d'itérations
-unsigned g2( vector<int>& v ) {
-    unsigned nbIterations = 0;
-    for(size_t i = 0; i < v.size(); ++i) {
-        for(size_t j = v.size() - 1; j > 0; j /= 2 ) {
-            ++nbIterations;
-            v.at(i) += v.at(j);
-        }
-    }
-    return nbIterations;
 }
 
 /**
@@ -206,91 +148,17 @@ vector<int> random2( size_t N , int maxVal ) {
     return v;
 }
 
-vector<int> generateVector(size_t size) {
-    vector<int> v(size);
-
-    for (int i = 0; i < size; ++i) {
-        v[i] = i;
-    }
-
-    random_shuffle(v.begin(), v.end(), [](int i) {
-        return rand()%i;
-    });
-
-    return v;
-}
-
 int main() {
     const int NB_TESTS = 1000;
 
     //initialisation du générateur aléatoire
     srand (time(NULL));
-    
-    testTrier(8, 2, NB_TESTS);
 
-    cout << "\n--------------\n";
+    testTrier(8, 2);
 
-    testChercherSiContient(10, 2, NB_TESTS);
+    cout << endl;
+    testChercherSiContient(14, 2, NB_TESTS);
 
-    cout << "\n--------------\n";
-
-    testG(8, 2, NB_TESTS);
-}
-
-void testTrier(int nbValeurs, int base, int nbTests) {
-    cout << fixed << setprecision(2);
-
-    cout << "Fonction : trier()\n";
-    for (int i = 1; i <= nbValeurs; ++i) {
-        vector<int> v = generateVector((size_t)pow(base, i));
-        double averageIterations = 0.;
-
-        cout << "Iterations pour n = " << v.size() << " (" << base << "^" << i  << ") : ";
-        for (int j = 1; j <= nbTests; ++j) {
-            unsigned nbIterations = trier2(v);
-
-            averageIterations += (double)nbIterations / nbTests;
-        }
-
-        cout << averageIterations << endl;
-    }
-}
-
-void testChercherSiContient(int nbValeurs, int base, int nbTests) {
-    cout << fixed << setprecision(2);
-
-    cout << "Fonction : chercherSiContient2()\n";
-    for (int i = 1; i <= nbValeurs; ++i) {
-        vector<int> v = generateVector((size_t)pow(base, i));
-        sort(v.begin(), v.end());
-
-        double averageIterations = 0.;
-        cout << "Iterations pour n = " << v.size() << " (" << base << "^" << i  << ") : ";
-        for (int j = 1; j <= nbTests; ++j) {
-            unsigned nbIterations = chercherSiContient2(v, rand() % v.size());
-
-            averageIterations += (double)nbIterations / nbTests;
-        }
-
-        cout << averageIterations << endl;
-    }
-}
-
-void testG(int nbValeurs, int base, int nbTests) {
-    cout << fixed << setprecision(2);
-
-    cout << "Fct : g()\n";
-    for (int i = 1; i <= nbValeurs; ++i) {
-        vector<int> v = generateVector((size_t)pow(base, i));
-
-        double averageIterations = 0.;
-        cout << "Iterations pour n = " << v.size() << " (" << base << "^" << i  << ") : ";
-        for (int j = 1; j <= nbTests; ++j) {
-            unsigned nbIterations = g2(v);
-
-            averageIterations += (double)nbIterations / nbTests;
-        }
-
-        cout << averageIterations << endl;
-    }
+    cout << endl;
+    testG(8, 2);
 }
