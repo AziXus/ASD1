b949549d801556f4277dccd3dfc8dfff43222851
Naludrag <stephanecarvalho81@gmail.com>
2019-03-06 18:04:35 +0100

    Update main.cpp

--- Labo02_ASD1/main.cpp
+++ Labo02_ASD1/main.cpp
@@ -1,25 +1,69 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.cpp
- * Author: stéphane
- *
- * Created on March 6, 2019, 3:08 PM
- */
-
 #include <cstdlib>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include "pieces.h"
 
 using namespace std;
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+void tournerPiece(Piece piece)
+{
+    rotate(piece.begin(),piece.end(),piece.end());
+}
 
-    return 0;
+bool estCompatible(Piece piece1, Piece piece2)
+{
+    for(auto i = piece1.begin(); i < piece1.end();i++)
+    {
+        for(auto j = piece2.begin(); j < piece2.end();j++)
+        {
+            if(i%2 == 0)
+            {
+                if(*i == *j - 1)
+                {
+                  return true;
+                }   
+            }
+            else
+            {
+                if(*i - 1 == *j)
+                {
+                  return true;
+                } 
+            }
+        }
+    }
+    return false;
+}
+
+string poserPiece(Piece piece, int orientation, Pieces piecesDisponible)
+{
+    if(piecesDisponible.size() == 0)
+    {
+        return char('A' + orientation);
+    }
+    else{
+        for(int i = 0; i < piecesDisponible.size(); i++)
+        {
+            if(estCompatible(piecesDisponible.at(i), piece))
+            {
+                for(int k = 0; i < k; i++)
+                {
+                    estCompatible(piecesDisponible.at(i).at(1), piece.at(3));
+                }
+            }
+            else
+            {
+                
+            }
+        }
+    }
+}
+
+
+int main() {
+    
+
+    return EXIT_SUCCESS;
 }
 
