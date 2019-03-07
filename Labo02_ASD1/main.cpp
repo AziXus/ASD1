1c75dbb5069e6d6a7cd32e7b319c92b1ca2e2e81
unknown <diegovillagrasa@gmail.com>
2019-03-07 09:13:36 +0100

    Ajout de la fonction verifierPiece qui remplace estCompatible qui devient un check plus général

--- Labo02_ASD1/main.cpp
+++ Labo02_ASD1/main.cpp
@@ -8,67 +8,98 @@ using namespace std;
 //Enum des cotes d'une piece
 enum SIDES {HAUT, DROITE, BAS, GAUCHE};
 
-const short MAX_ROTATIONS = 3;
+const unsigned short MAX_ROTATIONS = 3;   // Nombre de rotations max d'une pièce
+const unsigned short MAX_PAIR = 7;        // Id de AttachementType qui fait encore partit d'une paire
+const unsigned short PAIR_OFFSET = 1;     // Offset de chaque paire dans AttachementType
 
-bool estCompatible(const Piece& lhs, SIDES lSide, const Piece& rhs, SIDES rSide){
-   if(rhs[rSide] > 7)
+
+/**
+ * @brief Verifie si deux pièces sont compatible
+ * 
+ * @param lhs première pièce
+ * @param lSide côté a comparer de la première pièce
+ * @param rhs deuxième pièce
+ * @param rSide côté a comparer de la deuxième pièce
+ * @return true si les pièces sont compatibles
+ * @return false si les pièces ne sont pas compatibles
+ */
+bool verifierPieces(const Piece& lhs, SIDES lSide, const Piece& rhs, SIDES rSide){
+   //Si le coté n'a pas de paire
+   if(rhs[rSide] > MAX_PAIR)
       return false;
-   if (lhs[lSide] % 2 == 0) {
-     return lhs[lSide] + 1 == rhs[rSide];
-   }
-   return lhs[lSide] - 1 == rhs[rSide];
+   //Si c'est la partie gauche de la paire
+   if (lhs[lSide] % 2 == 0)
+     return lhs[lSide] + PAIR_OFFSET == rhs[rSide];
+   //La droite
+   return lhs[lSide] - PAIR_OFFSET == rhs[rSide];
 }
 
-
-bool afficherPiece(Piece piece){
+/**
+ * @brief Affiche l'id et l'orientation d'une pièce
+ *        A partir de la variable globale PIECES
+ * 
+ * @param piece pièce à afficher
+ */
+void afficherPiece(Piece& piece){
    Pieces def = PIECES;
-   for(auto i = def.begin(); i != def.end(); i++)
-   {
-      for(size_t j = 0; j <= MAX_ROTATIONS; j++)
-      {
-         if(piece == *i){
+   for(auto i = def.begin(); i != def.end(); i++) {
+      for(size_t j = 0; j <= MAX_ROTATIONS; j++) {
+         if(piece == *i) {
             cout << distance(def.begin(), i)+1;
             cout << (char) ((int) 'a' + j) << " ";
-            return true;
+            return;
          }
          rotate(i->begin(), i->begin() + 1, i->end());
       }
    }
-   return false;
 }
 
-void afficherPieces(Pieces pieces){ 
+/**
+ * @brief Affiche un vecteur de Piece (Pieces)
+ * 
+ * @param pieces vecteur de Piece
+ */
+void afficherPieces(Pieces& pieces){ 
    for(auto i = pieces.begin(); i != pieces.end(); i++)
-   {
       afficherPiece(*i);
-   }
    cout << endl;
 }
 
+/**
+ * @brief Verifie si une pièce peut être posée
+ * 
+ * @param used Pièces utilisées
+ * @param piece Piece à poser
+ * @return true Si la pièce peut être posée
+ * @return false Si la pièce ne peut pas être posée
+ */
+bool estCompatible(Pieces& used, Piece& piece){
+   return used.size() == 0 || 
+         (used.size() < 3 && verifierPieces(used.back(), SIDES::DROITE, piece, SIDES::GAUCHE)) || 
+         (used.size() >= 3 && used.size() % 3 == 0 && verifierPieces(used.at(used.size()-3), SIDES::BAS, piece, SIDES::HAUT)) ||
+         (used.size() >= 3 && used.size() % 3 != 0 && verifierPieces(used.at(used.size()-3), SIDES::BAS, piece, SIDES::HAUT) && verifierPieces(used.back(), SIDES::DROITE, piece, SIDES::GAUCHE));
+}
+
 void poserPiece(Pieces& used, Pieces& disponibles){
-   //On fake qu'on a pas trouvé mais on print quand meme l'ordre
-   //Comme ca il calcul les autres solutions
+   /*Si la dernière Piece à été posée, afficher les pièces utilisées 
+   et return pour tester les autres solutions*/
    if(disponibles.size() == 0){
       afficherPieces(used);
       return;
    }
-   for(auto i = disponibles.begin(); i != disponibles.end(); i++)
-   {
-      for(size_t j = 0; j <= MAX_ROTATIONS; j++)
-      {
-         if(used.size() == 0 || 
-         (used.size() < 3 && estCompatible(used.back(), SIDES::DROITE, *i, SIDES::GAUCHE) || 
-         (used.size() >= 3 && used.size() % 3 == 0 && estCompatible(used.at(used.size()-3), SIDES::BAS, *i, SIDES::HAUT)) ||
-         (used.size() >= 3 && used.size() % 3 != 0 && estCompatible(used.at(used.size()-3), SIDES::BAS, *i, SIDES::HAUT) && estCompatible(used.back(), SIDES::DROITE, *i, SIDES::GAUCHE)))){
+   //On teste chaque pièces disponibles
+   for(auto i = disponibles.begin(); i != disponibles.end(); i++) {
+      //On tourne chaque pièces
+      for(size_t j = 0; j <= MAX_ROTATIONS; j++) {
+         if(estCompatible(used, *i)){
             used.push_back(*i);
             disponibles.erase(i);
-            //On pose la piece suivante
             poserPiece(used, disponibles);
             //Ca marche pas on remet comme avant
             disponibles.insert(i, used.back());
             used.pop_back();
          }
-         //On shift a gauche (comme le site)
+         //On shift a gauche pour simuler une rotation de la pièce (comme le site)
          rotate(i->begin(), i->begin() + 1, i->end()); 
       }
    }
