774e7b0cbeea02c23068b15338b33cd939addd95
Naludrag <stephanecarvalho81@gmail.com>
2019-05-06 10:09:29 +0200

    Update Pile_2.h

--- ASD1_Labo6/Pile_2.h
+++ ASD1_Labo6/Pile_2.h
@@ -1,3 +1,11 @@
+/**
+\file Pile.h
+\author Stéphane Teixeira Carvalho, Diego Villagrasa, Robin Müller
+\date 06 mai 2019
+Labo 6 : Création d'une classe générique Pile pour qu'elle passe le codecheck.
+Cette classe met en oeuvre le comportement d'une stack (pile) en utilisant le concept d'allocation dynamique.
+L'allocation est effectuée avec ::operator new tandis que la construction utilise le new (ptr) ou ptr est un pointeur vers un emplacement mémoire déjà réservé.
+*/
 #ifndef Pile_hpp
 #define Pile_hpp
 
@@ -26,42 +34,66 @@ private:
     const size_t CAPACITE;
 
 public:
+    /**
+     * Constructeur spécifique de la classe Pile n'indiuqant que la taille
+     * @param _capacite size_t indiquant la capacite de la pile
+     */
     Pile(size_t _capacite)
             : taille(0), CAPACITE(_capacite)
     {
+        //allocation en mémoire de la place que va prendre la pile
         donnees = (pointer)::operator new(CAPACITE * sizeof(value_type));
     }
-
+            
+    /**
+     * Destructeur de la classe Pile
+     */
     ~Pile()
     {
+        //destruction des valeurs contenues dans la pile
         for (unsigned i = 0 ; i < taille; ++i) {
             donnees[i].~value_type();
         }
-
+        //destruction en mémoire de la pile
         ::operator delete(donnees);
     }
-
+    
+    /**
+     * Constucteur par copie de la classe Pile
+     * @param other Pile allant être copié
+     */
     Pile(const Pile& other)
             : taille(other.taille), CAPACITE(other.CAPACITE)
     {
+        //Allocation de la place en mémoire que va prendre la pile
         donnees = (pointer)::operator new(CAPACITE * sizeof(value_type));
         for(unsigned i = 0; i < taille; ++i) {
+            //on crée les objets de type value_type avec les valeurs de other
             new(donnees + i) value_type(other.donnees[i]);
         }
     }
-
+    /**
+     * Permet d'ajouter une nouvelle valeur à la pile
+     * @param v valeur de type value_type à ajouter
+     */
     void empiler(const value_type& v)
     {
+        //créer un nouvel objet de type value_type en mémoire selon la taille
         new(donnees + taille) value_type(v);
         ++taille;
     }
-
+    /**
+     * Enlève la valeur au sommet de la pile. Fait appel au destructeur de value_type
+     */
     void depiler()
     {
         donnees[taille - 1].~value_type();
         --taille;
     }
-
+    /**
+     * Permet de donner la valeur du sommet de la pile
+     * @return const_reference étant la valeur au sommet de la pile
+     */
     const_reference sommet() const
     {
         return donnees[taille - 1];
