4790ce2e0a42dd274bc87ad699b821e7e89f7a1f
Naludrag <stephanecarvalho81@gmail.com>
2019-06-16 17:08:12 +0200

    Update main.cpp

--- ASD1_Labo10/main.cpp
+++ ASD1_Labo10/main.cpp
@@ -1,25 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.cpp
- * Author: stéphane
- *
- * Created on June 5, 2019, 2:55 PM
- */
-
 #include <cstdlib>
+#include <map>
+
 
 using namespace std;
 
-/*
- * 
- */
-int main(int argc, char** argv) {
-
+int main() {
+    map parent<1,2>;
     return 0;
 }
 
