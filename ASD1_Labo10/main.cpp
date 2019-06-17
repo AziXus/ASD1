d1bfe58d0846166cfd79016a8ee35a791b79c9de
Naludrag <stephanecarvalho81@gmail.com>
2019-06-17 21:29:46 +0200

    Update main.cpp

--- ASD1_Labo10/main.cpp
+++ ASD1_Labo10/main.cpp
@@ -1,10 +1,14 @@
-
+/**
+\file main.cpp
+\author Stéphane Teixeira Carvalho, Diego Villagrasa, Robin Müller
+\date 17 Juin 2019
+Labo 10 : Création d'un programme permettant de générer la séquence la plus courte pour résoudre un taquin 3x3.
+La solution donnera tout les déplacements à effectuer avec la case vide(0). Les cases sont notées de 0 à 8.
+*/
 #include <cstdlib>
 #include <iostream>
 #include <string>
 #include <vector>
-#include <list>
-#include <map>
 #include <queue>
 #include <algorithm>
 #include <unordered_set>
@@ -21,8 +25,14 @@ struct Sommet {
     string etat;
     string chemin;
 };
-
-Sommet ajoutVoisin(const Sommet& noeud, size_t pos, Deplacement deplacement){
+/**
+ * Déplace le noeud passé en paramètres vers une postion voisin donné
+ * @param noeud sommet de type constant étant le noeud à deplacer
+ * @param pos size_t indiqaunt la postion du noeud dans le taquin
+ * @param deplacement variable de type Deplacement permettant d'indiquer au noeud vers quelle voisin se déplacer
+ * @return le nouvelle position du sommet dans le taquin
+ */
+Sommet deplacementVoisin(const Sommet& noeud, size_t pos, Deplacement deplacement){
     Sommet nouveauNoeud;
     nouveauNoeud.etat = noeud.etat;
     switch(deplacement){
@@ -46,7 +56,11 @@ Sommet ajoutVoisin(const Sommet& noeud, size_t pos, Deplacement deplacement){
     }
     return nouveauNoeud;
 }
-
+/**
+ * Permet de trouver les voisins d'un noeud
+ * @param noeud variable de type Sommet étant le noeud auquel il faut trouvé ses voisions
+ * @return un vecteur de Sommet contenant les différents voisins du sommet
+ */
 vector<Sommet> trouverVoisins(const Sommet& noeud) {
     vector<Sommet> etatsVoisin;
 
@@ -56,27 +70,33 @@ vector<Sommet> trouverVoisins(const Sommet& noeud) {
     //Recherche des mouvements possible
     //Possibilité de mouvement vers le haut si on est pas sur la première ligne(pos plus loin que la 3ème case)
     if (pos >= NB_COLONNES) {     
-        etatsVoisin.push_back(ajoutVoisin(noeud, pos, HAUT));
+        etatsVoisin.push_back(deplacementVoisin(noeud, pos, HAUT));
     }
 
     //Possibilté de mouvement vers le bas donc est pas sur la dernière ligne plus petit que 6
     if (pos < (NB_COLONNES - 1) * NB_LIGNES) {
-        etatsVoisin.push_back(ajoutVoisin(noeud, pos, BAS));
+        etatsVoisin.push_back(deplacementVoisin(noeud, pos, BAS));
     }
 
     //Possibilté de mouvement vers la droite si la pos est dans la colonne 0 ou 1(pos % NB_COLONNES)
     if (pos % NB_COLONNES <= 1) {
-        etatsVoisin.push_back(ajoutVoisin(noeud, pos, DROITE));
+        etatsVoisin.push_back(deplacementVoisin(noeud, pos, DROITE));
     }
 
     //Possibilté de mouvement vers la gauche si la pos est dans la colonne 1 ou 2(pos % NB_COLONNES)
     if (pos % NB_COLONNES >= 1) {
-        etatsVoisin.push_back(ajoutVoisin(noeud, pos, GAUCHE));
+        etatsVoisin.push_back(deplacementVoisin(noeud, pos, GAUCHE));
     }
 
     return etatsVoisin;
 }
 
+/**
+ * Permet le parcours en largeur d'un graphe en donnant son sommet de manière récursive
+ * @param sommet string contenant le sommet à partir duquel démarrer le parcours
+ * @param chemin string contenant le chemin parcouru dans le graphe(contient les déplacments de la case 0)
+ * @return vrai si le parcours à atteint l'état 01234578, faux sinon
+ */
 bool BFS(const string& sommet, string& chemin) {
     chemin = "";
 
@@ -88,7 +108,7 @@ bool BFS(const string& sommet, string& chemin) {
     queue<Sommet> q;
     q.push(noeudDepart);
 
-    //Stocke les parents dans un ordered_set pour avoir une recherche de complexité constante
+    //Stocke les parents dans un unordered_set pour avoir une recherche de complexité constante
     unordered_set<string> parents;
 
     while (!q.empty()) {
@@ -96,7 +116,7 @@ bool BFS(const string& sommet, string& chemin) {
         q.pop();
 
         parents.insert(parent.etat);
-
+        
         auto voisins = trouverVoisins(parent);
 
         for (const auto& voisin : voisins) {
@@ -118,13 +138,21 @@ bool BFS(const string& sommet, string& chemin) {
 
 int main() {
     string source, chemin;
+    bool entreeValide = false;
     
-    cout << "Etat du taquin : ";
-    getline(cin, source);
-
-    //Ajouter fonction de vérification et de nettoyage de la saisie (espace)
-    //On enlève les eventuelles espaces
-    source.erase(remove_if(source.begin(), source.end(), ::isspace), source.end());
+    
+    do {
+        cout << "Etat du taquin : ";
+        getline(cin, source);
+        //Ajouter fonction de vérification et de nettoyage de la saisie (espace)
+        //On enlève les eventuelles espaces
+        source.erase(remove_if(source.begin(), source.end(), ::isspace), source.end());
+        entreeValide = (source.length() == 9 ? true : false);
+        if(!entreeValide) {
+            cin.clear();
+            cout << "Entree invalide. La chaine doit contenir au minimum 9 caracteres " << endl;
+        }
+    } while (!entreeValide);    
 
     if (BFS(source, chemin)) {
         cout << "Le chemin suivant a ete trouve : ";
