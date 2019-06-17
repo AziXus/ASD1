9128d699fb1aea6a16425c898334bb7bd475f6f5
Naludrag <stephanecarvalho81@gmail.com>
2019-06-17 19:30:00 +0200

    Update main.cpp

--- ASD1_Labo10/main.cpp
+++ ASD1_Labo10/main.cpp
@@ -15,53 +15,60 @@ const char EMPLACEMENT_VIDE = '0';
 const uint8_t NB_COLONNES = 3;
 const uint8_t NB_LIGNES = 3;
 const string ETAT_CIBLE = "012345678";
+enum Deplacement {HAUT, BAS, DROITE, GAUCHE};
 
 struct Sommet {
     string etat;
     string chemin;
 };
 
+Sommet ajoutVoisin(const Sommet& noeud, size_t pos, Deplacement deplacement){
+    Sommet nouveauNoeud;
+    nouveauNoeud.etat = noeud.etat;
+    switch(deplacement){
+        //nouveauNoeud.etat[pos - NB_COLONNES] nous donne la case situé au dessus du 0 avec laquelle on peut échanger de place
+        case HAUT   : swap(nouveauNoeud.etat[pos], nouveauNoeud.etat[pos - NB_COLONNES]);
+                      nouveauNoeud.chemin = noeud.chemin + to_string(pos - NB_COLONNES);
+                      break;
+        case BAS    : swap(nouveauNoeud.etat[pos], nouveauNoeud.etat[pos + NB_COLONNES]);
+                      nouveauNoeud.chemin = noeud.chemin + to_string(pos + NB_COLONNES);
+                      break;
+        case DROITE : swap(nouveauNoeud.etat[pos], nouveauNoeud.etat[pos + 1]);
+                      nouveauNoeud.chemin = noeud.chemin + to_string(pos + 1);
+                      break;
+        case GAUCHE : swap(nouveauNoeud.etat[pos], nouveauNoeud.etat[pos - 1]);
+                      nouveauNoeud.chemin = noeud.chemin + to_string(pos - 1);
+                      break;
+        default     : cout << "INCONNUE";
+    }
+    return nouveauNoeud;
+}
+
 vector<Sommet> trouverVoisins(const Sommet& noeud) {
     vector<Sommet> etatsVoisin;
 
-    //On trouve l'emplacement du 0
+    //On trouve l'emplacement du 0 dans la chaîne
     size_t pos = noeud.etat.find(EMPLACEMENT_VIDE);
 
-    //Recherche les mouvements possible
-    //Haut donc si on est pas sur la première ligne
-    if (pos >= NB_COLONNES) {
-        Sommet nouveauNoeud;
-        nouveauNoeud.etat = noeud.etat;
-        swap(nouveauNoeud.etat[pos], nouveauNoeud.etat[pos - NB_COLONNES]);
-        nouveauNoeud.chemin = noeud.chemin + to_string(pos - NB_COLONNES);
-        etatsVoisin.push_back(nouveauNoeud);
+    //Recherche des mouvements possible
+    //Possibilité de mouvement vers le haut si on est pas sur la première ligne(pos plus loin que la 3ème case)
+    if (pos >= NB_COLONNES) {     
+        etatsVoisin.push_back(ajoutVoisin(noeud, pos, HAUT));
     }
 
     //Bas donc est pas sur la dernière ligne
     if (pos < (NB_COLONNES - 1) * NB_LIGNES) {
-        Sommet nouveauNoeud;
-        nouveauNoeud.etat = noeud.etat;
-        swap(nouveauNoeud.etat[pos], nouveauNoeud.etat[pos + NB_COLONNES]);
-        nouveauNoeud.chemin = noeud.chemin + to_string(pos + NB_COLONNES);
-        etatsVoisin.push_back(nouveauNoeud);
+        etatsVoisin.push_back(ajoutVoisin(noeud, pos, BAS));
     }
 
     //Droite
     if (pos % NB_COLONNES <= 1) {
-        Sommet nouveauNoeud;
-        nouveauNoeud.etat = noeud.etat;
-        swap(nouveauNoeud.etat[pos], nouveauNoeud.etat[pos + 1]);
-        nouveauNoeud.chemin = noeud.chemin + to_string(pos + 1);
-        etatsVoisin.push_back(nouveauNoeud);
+        etatsVoisin.push_back(ajoutVoisin(noeud, pos, DROITE));
     }
 
     //Gauche
     if (pos % NB_COLONNES >= 1) {
-        Sommet nouveauNoeud;
-        nouveauNoeud.etat = noeud.etat;
-        swap(nouveauNoeud.etat[pos], nouveauNoeud.etat[pos - 1]);
-        nouveauNoeud.chemin = noeud.chemin + to_string(pos - 1);
-        etatsVoisin.push_back(nouveauNoeud);
+        etatsVoisin.push_back(ajoutVoisin(noeud, pos, GAUCHE));
     }
 
     return etatsVoisin;
@@ -109,7 +116,7 @@ bool BFS(const string& sommet, string& chemin) {
 
 int main() {
     string source, chemin;
-
+    
     cout << "Etat du taquin : ";
     getline(cin, source);
 
